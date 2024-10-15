833d885f7a3d8bf9e9c62fcf50c103a1843da1de
jjketten <jjketten21@gmail.com>
2024-10-15 06:00:21 +0000

    Removed some commented code and currently unused imports

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,11 @@
 package com.example.demo;
 import java.io.FileInputStream;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-//import com.example.FirebaseInitialize;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
@@ -24,17 +20,11 @@ import jakarta.annotation.PostConstruct;
 public class DemoApplication {
     private static final String SECRETFILE = "serviceAccountKey.json";
     private static final String SECRETDIRVAR = "SECRET_LOCATION";
-    // @Value("${SECRETDIRVAR}")
-    // private static String secretLocation;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    // @GetMapping("/hello")
-    // public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-    //     return String.format("Hello %s!", name);
-    // }
     
 
     @PostConstruct
@@ -49,7 +39,6 @@ public class DemoApplication {
         FileInputStream serviceAccount = new FileInputStream(secretLocation.concat(SECRETFILE));
         FirebaseOptions options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            //.setProjectId("study-group-finder")
             .build();
 
         FirebaseApp.initializeApp(options);
@@ -57,8 +46,6 @@ public class DemoApplication {
 
         } catch (Exception e) {
             System.out.println("FATAL ERROR: Firebase connection could not be initialized or file handling issue.");
-            //System.out.println(e.getMessage());
-            //TODO: handle exception
             e.printStackTrace();
         }
         Firestore db = FirestoreClient.getFirestore();
