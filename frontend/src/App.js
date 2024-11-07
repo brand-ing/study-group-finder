0ee98d66b57aeb764fe16fc9d035e3187d20c3a9
brand-ing <epicemailtime@gmail.com>
2024-11-07 05:42:32 -0500

    add changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,27 @@ import Login from './components/Login';
 import Register from './components/Register';
 import './components/styles.css';
 import LandingPage from './components/LandingPage';
+import ProfileMaker from './components/ProfileMaker';
+import Dashboard from './components/Dashboard';
+import MyPage from './components/myPage';
 
 function App() {
+  const handleLoginSuccess = () => {
+    console.log("User logged in successfully!");
+    // Add any additional logic you need here
+  };
+
   return (
     <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login onLogin={handleLoginSuccess} />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/profile-maker" element={<ProfileMaker />} />
+          <Route path="/dashboard/@me" element={<MyPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+
         </Routes>
       </div>  
     </Router>
